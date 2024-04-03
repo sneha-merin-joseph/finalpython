2bdc394ea7559803397ddf103ed58b9fd73a7e26
unknown <snehajoseph@580@gmail.com>
2024-04-03 11:07:48 +0530

    add

---
+++ finalproject/finalapp/urls.py
@@ -0,0 +1,19 @@
+
+
+
+from django.urls import path
+from . import views
+urlpatterns = [
+
+    path('',views.demo,name='demo'),
+    path('register',views.register,name='register'),
+    path('logout', views.logout, name='logout'),
+    path('login',views.login,name='login'),
+    path('detail', views.detail, name='detail'),
+    path('add',views.add_movie,name='add_movie'),
+    # path('add',views.add_movi,name='add_movi'),
+    # path('detail/<int:movie_id>/', views.detail,name='detail'),
+    path('update/<int:id>/', views.update, name='update'),
+    # path('update/<int:id>/',views.update,name='update'),
+    path('delete/<int:id>/',views.delete,name='delete'),
+]
